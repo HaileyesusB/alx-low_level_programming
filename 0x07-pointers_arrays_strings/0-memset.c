508d215b02e32dfbd8b076b4b627635cfcaddc38
HaileyesusB <88820045+HaileyesusB@users.noreply.github.com>
2022-06-21 17:54:13 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
 /**
- *_memset - fills a memory block with a constant byte
- * @s: address to memory block
- * @b: char to be used
- * @n: number of bytes to be used
- *
- * Return: pointer to the memory block
+ * _memset - sets memory to array
+ * @s: array to set
+ * @b: value to set it as
+ * @n: n amount of times
+ * Return: char value of s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n)
-	{
-		s[n - 1] = b;
-		n--;
-	}
+	unsigned int count;
+
+	for (count = 0; count < n; count++)
+		s[count] = b;
 	return (s);
 }
