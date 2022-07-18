002c915cc3632841845b0fd6e17c585bfdceffbe
Haileyesus Belew <88820045+HaileyesusB@users.noreply.github.com>
2022-07-18 13:56:13 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,19 @@
+#include <stdio.h>
 #include "lists.h"
 /**
- * listint_len - Number of element in a list
- * @h:pointer to the head of the list
- * Return: number of elements
- */
-
+ ** listint_len - Returns the number of elements
+ **               in a linked listint_t list.
+ ** @h: A pointer to the head of the listint_t list.
+ **
+ ** Return: The number of elements in the listint_t list.
+ **/
 size_t listint_len(const listint_t *h)
 {
-const listint_t *current = h;
-int number_node = 0;
-while (current != NULL)
+size_t all_nodes = 0;
+while (h)
 {
-current  = current->next;
-number_node++;
+all_nodes++;
+h = h->next;
 }
-return (number_node);
+return (all_nodes);
 }
