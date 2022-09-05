0c0ebeadf2f202881f55a985cdea06e4ebe630a6
Haileyesus Belew <88820045+HaileyesusB@users.noreply.github.com>
2022-09-05 21:13:28 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/betascribbles/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P .. https://raw.githubusercontent.com/YosephMekonnenAbebe/alx-low_level_programming/main/0x18-dynamic_libraries/libinjection.so
+export LD_PRELOAD="$PWD/../libinjection.so"
